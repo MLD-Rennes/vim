09273285b091e385a0c013ee09abc18867461ae0
mathledantec <matheo.le-dantec@etudiant.univ-rennes.fr>
2026-04-08 15:20:38 +0200

    Deleted plugins files.

--- nvim/after/plugin/telescope.lua
+++
@@ -1,9 +0,0 @@
-local builtin = require('telescope.builtin')
-local options = { noremap = true }
-local map = vim.api.nvim_set_keymap
-
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
