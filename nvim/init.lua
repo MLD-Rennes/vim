248196969e33d13dcada73f6eb6d3bcf52120ec7
MLD-Rennes <matheo.le-dantec@etudiant.univ-rennes.fr>
2026-08-20 00:16:14 +0200

    Ajoute le plugin everforest (style)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 vim.cmd('filetype plugin on')
 require("core")
-require("filetypes")
-require("config.lazy")
-require('plugins')
+--require("filetypes")
+--require("config.lazy")
+--require('plugins')
 
---
+++ nvim/plugin/everforest.lua
@@ -0,0 +1,5 @@
+vim.pack.add { "https://github.com/neanias/everforest-nvim" }
+require("everforest").setup({
+  -- Your config here
+})
+vim.cmd([[colorscheme everforest]])
